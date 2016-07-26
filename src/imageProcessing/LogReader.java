f7a993265a92aa22f00c5c507dbda4cddeb43547
MSHAO1 <MSHAO1@MEDIXWS3.cstcis.cti.depaul.edu>
2016-07-26 16:21:59 -0500

    frames are aligned

--- src/imageProcessing/LogReader.java
+++ src/imageProcessing/LogReader.java
@@ -12,16 +12,19 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author MSHAO1
  */
 public class LogReader {
+
     private DataInputStream is = null;
     private final String inputDirectory;
-    
-     public LogReader(String source) {
+
+    public LogReader(String source) {
         inputDirectory = source;
         try {
             is = new DataInputStream(new FileInputStream(new File(inputDirectory + "\\log.dat")));
@@ -29,8 +32,22 @@ public class LogReader {
             ex.printStackTrace();
         }
     }
-     
-     public static boolean[] getMovingMatrix(){
-         
-     }
+
+    public int[] getMovingMatrix() {
+        int[] movingMatrix = new int[dto.Properties.imagecount];
+        try {
+            int i = 0;
+            while (is.available() > 0) {
+                is.readInt();//frame
+                is.readLong();//timeStamp
+                is.readInt();//x
+                is.readInt();//y
+                movingMatrix[i++] = is.readInt();
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return movingMatrix;
+    }
 }
